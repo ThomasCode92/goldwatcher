e51987c42a1b33853a33d9ad53bf23e90a39dc99
Thomas <thomascode92@hotmail.com>
2024-07-01 08:03:10 +0200

    Writing a Function that will Refresh all Gold Price Information

--- ui.go
+++ ui.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
@@ -30,4 +31,14 @@ func (app *Config) makeUI() {
 	// add container to the window
 	finalContent := container.NewVBox(priceContent, toolbar, tabs)
 	app.MainWindow.SetContent(finalContent)
+}
+
+func (app *Config) refreshPriceContent() {
+	open, current, change := app.GetPriceText()
+	app.PriceContainer.Objects = []fyne.CanvasObject{open, current, change}
+	app.PriceContainer.Refresh()
+
+	chart := app.getChart()
+	app.PriceChartContainer.Objects = []fyne.CanvasObject{chart}
+	app.PriceChartContainer.Refresh()
 }
