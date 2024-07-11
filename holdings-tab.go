2d74d5bdcd76b7647e18cc6453f1795351c088c8
Thomas <thomascode92@hotmail.com>
2024-07-11 21:02:46 +0200

    Getting the Slice of Holdings & Testing the 'currentHoldings' and 'getHoldingSlice' functions

--- holdings-tab.go
+++ holdings-tab.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"goldwatcher/repository"
+	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/widget"
@@ -17,6 +19,26 @@ func (app *Config) getHoldingsTable() *widget.Table {
 
 func (app *Config) getHoldingSlice() [][]interface{} {
 	var slice [][]interface{}
+
+	holdings, err := app.currentHoldings()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	slice = append(slice, []interface{}{"ID", "Amount", "Price", "Date", "Delete"})
+
+	for _, h := range holdings {
+		var currentRow []interface{}
+
+		currentRow = append(currentRow, strconv.FormatInt(h.ID, 10))
+		currentRow = append(currentRow, fmt.Sprintf("%d toz", h.Amount))
+		currentRow = append(currentRow, fmt.Sprintf("$%.2f", float32(h.PurchasePrice/1000)))
+		currentRow = append(currentRow, h.PurchaseDate.Format("2006-01-02"))
+		currentRow = append(currentRow, widget.NewButton("Delete", func() {}))
+
+		slice = append(slice, currentRow)
+	}
+
 	return slice
 }
 
