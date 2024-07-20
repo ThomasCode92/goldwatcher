0a4cbd552582a277adef28cf4797165342484ce1
Thomas <thomascode92@hotmail.com>
2024-07-20 14:46:47 +0200

    Enabling the Preferences Icon in the Toolbar

--- toolbar.go
+++ toolbar.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -16,7 +17,11 @@ func (app *Config) getToolBar() *widget.Toolbar {
 		widget.NewToolbarSpacer(),
 		widget.NewToolbarAction(theme.DocumentCreateIcon(), app.addHoldingsDialog),
 		widget.NewToolbarAction(theme.ViewRefreshIcon(), app.refreshPriceContent),
-		widget.NewToolbarAction(theme.SettingsIcon(), func() {}),
+		widget.NewToolbarAction(theme.SettingsIcon(), func() {
+			w := app.showPreferences()
+			w.Resize(fyne.NewSize(300, 200))
+			w.Show()
+		}),
 	)
 
 	return toolbar
@@ -83,4 +88,25 @@ func (app *Config) addHoldingsDialog() {
 	//size and show the dialog
 	addForm.Resize(fyne.Size{Width: 400})
 	addForm.Show()
+}
+
+func (app *Config) showPreferences() fyne.Window {
+	win := app.App.NewWindow("Preferences")
+
+	lbl := widget.NewLabel("Preferred Currency")
+	cur := widget.NewSelect([]string{"USD", "EUR", "GBP"}, func(value string) {
+		currency = value
+		app.App.Preferences().SetString("currency", value)
+	})
+	cur.Selected = currency
+
+	btn := widget.NewButton("Save", func() {
+		win.Close()
+		app.refreshPriceContent()
+	})
+	btn.Importance = widget.HighImportance
+
+	win.SetContent(container.NewVBox(lbl, cur, btn))
+
+	return win
 }
